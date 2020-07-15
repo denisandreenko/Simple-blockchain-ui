3c7e0857a16c05f99b6fbd7cf6392381b04226ad
Denis Andreenko <andreenkodn@gmail.com>
2020-07-15 21:59:03 +0300

    fix: add blockchain ui

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+
+import { Component, OnInit } from '@angular/core';
+import { BlockchainService } from './services/blockchain.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  public blockchain;
+
+  constructor(private blockchainService: BlockchainService) {
+    this.blockchain = blockchainService.blockchainInstance;
+  }
+
+  ngOnInit() {
+  }
+
+  thereArePendingTransactions() {
+    return this.blockchain.pendingTransactions.length > 0;
+  }
+}
